637a5cdfd67e1267fe7c75a7983210c5aa0b0965
pmisur <misurpatricia@gmail.com>
2019-05-23 14:09:38 +0200

    Második fázis (gcc)

--- main.c
+++ main.c
@@ -13,8 +13,8 @@ typedef struct matrix {
 
 // Allocate a double type matrix
 
-void alloc_matrix(matrix_t* mat){
-    mat->m = calloc(mat->rows * mat->cols, sizeof(double));
+void alloc_matrix(matrix_t* mat) {
+    mat->m = (double*)malloc(mat->rows * mat->cols * sizeof(double));
 
     if(mat->m == NULL) {
         fprintf(stderr,"Matrix allocation error!\n");
@@ -55,29 +55,23 @@ matrix_t mult(matrix_t m1, matrix_t m2) {
     temp.cols = m2.cols;
 
     alloc_matrix(&temp);
-    alloc_matrix(&m1);
-    alloc_matrix(&m2);
 
     for(int i = 0; i < temp.rows; ++i) {
         for(int j = 0; j < temp.rows; ++j) {
             for(int k = 0; k < m1.cols; ++k){
 
                 temp.m[temp.cols*i + j] += m1.m[m1.cols*i + k] * m2.m[m2.cols*k + j];
-
-                //print_matrix(temp, "belsoeredemeny");
             }
         }
     }
-
-    free(temp.m);
     return temp;
 }
 
 
 void main(int argc, char *argv[]){
 
-    char* filename;
-    matrix_t mat1, mat2;
+    //char* filename;
+    
 
     if (argc != 5) {
         fprintf(stderr,
@@ -85,28 +79,36 @@ void main(int argc, char *argv[]){
         exit(-1);
     }
 
-    filename = argv[1], argv[2];
-    mat1.rows = atoi(argv[3]);
-    mat2.cols = atoi(argv[4]);
     
-    if(mat2.cols!=mat1.rows){
+    //filename = argv[1], argv[2];
+    int rows, cols;
+
+    rows = atoi(argv[3]);
+    cols = atoi(argv[4]);
+    
+    if(rows != cols) {
         printf("Matrix multiplication error. \n");
         exit(-1);
     }
-
-    FILE *f = fopen("filename", "r");
-
+    matrix_t mat1;
+  
+    mat1.rows = rows;
+    mat1.cols = cols;
+    alloc_matrix(&mat1);
     read_matrix(argv[1], mat1);
     print_matrix(mat1, "matrix1");
 
+    matrix_t mat2;
+    
+    mat2.rows = rows;
+    mat2.cols = cols;
+    alloc_matrix(&mat2);
     read_matrix(argv[2], mat2);
     print_matrix(mat2, "matrix2");
 
-    //matrix_t res = mult(mat1, mat2);
-    //print_matrix(res, "Solution");
-
+    /*matrix_t res;
+    res = mult(mat1, mat2);
+    print_matrix(res, "Solution");*/
     free(mat1.m);
     free(mat2.m);
-    //free(res.m);
-
-} 
+}
