d80603539f4a2892b7b280e908b786ae6541bafa
pmisur <misurpatricia@gmail.com>
2019-05-27 12:19:25 +0200

    Cmake files

---
+++ build/CMakeFiles/matrixmultiplication.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrixmultiplication.dir/main.c.o"
+  "matrixmultiplication.pdb"
+  "matrixmultiplication"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/matrixmultiplication.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
